f6c351d44d021b75f77f4baebbfe436ef9faa867
Hans-Peter Schadler <hps@abyle.org>
2018-12-23 17:45:05 +0100

    fix UT

--- riotclientv3/riotclient_test.go
+++ riotclientv3/riotclient_test.go
@@ -4,23 +4,29 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/torlenor/alolstats/riotclient/datadragon"
+	"github.com/torlenor/alolstats/riotclient/ratelimit"
+
 	"github.com/torlenor/alolstats/config"
 )
 
 func TestRiotClient(t *testing.T) {
+	httpClient := &http.Client{}
+	ddragon, _ := riotclientdd.New(httpClient, config.RiotClient{})
+	rateLimit, _ := riotclientrl.New()
 	client, err := NewClient(httpClient, config.RiotClient{}, ddragon, rateLimit)
 	if err == nil || client != nil {
 		t.Fatalf("Could get a new client even though APIVersion is missing from config")
 	}
-	client, err = NewClient(&http.Client{}, config.RiotClient{APIVersion: "v1"})
+	client, err = NewClient(&http.Client{}, config.RiotClient{APIVersion: "v1"}, ddragon, rateLimit)
 	if err == nil || client != nil {
 		t.Fatalf("Could get a new client even though Key is missing from config")
 	}
-	client, err = NewClient(&http.Client{}, config.RiotClient{APIVersion: "v1", Key: "abcd"})
+	client, err = NewClient(&http.Client{}, config.RiotClient{APIVersion: "v1", Key: "abcd"}, ddragon, rateLimit)
 	if err == nil || client != nil {
 		t.Fatalf("Could get a new client even though Region is missing from config")
 	}
-	client, err = NewClient(&http.Client{}, config.RiotClient{APIVersion: "v1", Key: "abcd", Region: "euw"})
+	client, err = NewClient(&http.Client{}, config.RiotClient{APIVersion: "v1", Key: "abcd", Region: "euw"}, ddragon, rateLimit)
 	if err != nil || client == nil {
 		t.Fatalf("Could not get a new client")
 	}
