6e92ac4e2eb91e4ff2dabc39182e865b2d666192
Hans-Peter Schadler <hps@abyle.org>
2018-12-26 14:00:15 +0100

    Riot API V4 Features Games UTs

--- riotclient/spectator.go
+++ riotclient/spectator.go
@@ -54,29 +54,30 @@ type CurrentGameInfoDTO struct {
 	GameLength        int64                       `json:"gameLength"`
 }
 
+// FeaturedGameInfoParticipantDTO contains participant information in a featured game
+type FeaturedGameInfoParticipantDTO struct {
+	ProfileIconID int    `json:"profileIconId"`
+	ChampionID    int    `json:"championId"`
+	SummonerName  string `json:"summonerName"`
+	Bot           bool   `json:"bot"`
+	Spell2ID      int    `json:"spell2Id"`
+	TeamID        int    `json:"teamId"`
+	Spell1ID      int    `json:"spell1Id"`
+}
+
 // FeaturedGameInfoDTO contains informations on one Featured Game
 type FeaturedGameInfoDTO struct {
-	GameID            int64  `json:"gameId"`
-	GameStartTime     int64  `json:"gameStartTime"`
-	PlatformID        string `json:"platformId"`
-	GameMode          string `json:"gameMode"`
-	MapID             int    `json:"mapId"`
-	GameType          string `json:"gameType"`
-	GameQueueConfigID int    `json:"gameQueueConfigId"`
-	Observers         struct {
-		EncryptionKey string `json:"encryptionKey"`
-	} `json:"observers"`
-	Participants []struct {
-		ProfileIconID int    `json:"profileIconId"`
-		ChampionID    int    `json:"championId"`
-		SummonerName  string `json:"summonerName"`
-		Bot           bool   `json:"bot"`
-		Spell2ID      int    `json:"spell2Id"`
-		TeamID        int    `json:"teamId"`
-		Spell1ID      int    `json:"spell1Id"`
-	} `json:"participants"`
-	GameLength      int           `json:"gameLength"`
-	BannedChampions []interface{} `json:"bannedChampions"`
+	GameID            int64                            `json:"gameId"`
+	GameStartTime     int64                            `json:"gameStartTime"`
+	PlatformID        string                           `json:"platformId"`
+	GameMode          string                           `json:"gameMode"`
+	MapID             int                              `json:"mapId"`
+	GameType          string                           `json:"gameType"`
+	GameQueueConfigID int                              `json:"gameQueueConfigId"`
+	Observers         ObserverDTO                      `json:"observers"`
+	Participants      []FeaturedGameInfoParticipantDTO `json:"participants"`
+	GameLength        int                              `json:"gameLength"`
+	BannedChampions   []BannedChampionDTO              `json:"bannedChampions"`
 }
 
 // FeaturedGamesDTO models the Riot API response for Features Games
